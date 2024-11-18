893d9a8d80df55ef0e343910fa8b0470995edcf2
Đoàn Ngọc Minh Quân <minhquandoanngoc@gmail.com>
2024-11-18 13:29:35 +0700

    [Create] OrderPage

---
+++ src/pages/order.tsx
@@ -0,0 +1,12 @@
+// I want to create a page that shows all the orders of the user
+export default function Order() {
+    return (
+        <div>
+            <h1>Order</h1>
+            <div>
+                <h2>Order 1</h2>
+            </div>
+        </div>
+    )
+}
+
